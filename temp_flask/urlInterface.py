5b37df79a0fe8fa5ce419d005a53e601857a3bba
jin65 <sub07209@gmail.com>
2020-06-10 09:03:01 +0900

    temporary flask directory

---
+++ temp_flask/urlInterface.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import os
+from flask import Flask
+from flask import render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS=set('txt')
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/fileUpload', methods=['GET','POST'])
+def upload_file():
+	if request.method=='POST':
+		f=request.files['file']
+		#텍스트파일 flask/upload에 저장
+		f.save("./upload/"+secure_filename(f.filename))
+		return 'uploads complete'
+
+if __name__=='__main__':
+	app.run(debug=False)
